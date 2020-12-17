32e7e665bd85d61f388cb000e0058f2e0bbc690e
Peter Komaromi <kompeet1@gmail.com>
2020-12-17 16:13:39 +0000

    Added and registered CategoryAdmin and ProductsAdmin models

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,25 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category) 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'year',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+# Register classes with models
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin) 
--- products/models.py
+++ products/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your models here.
 # Base of the models from the Boutique Ado project
-
+# Metaclass provided to fix the spelling, declare that the plural version of Category is not Categorys 
 
 
 # Categories with a name and a friendly name for frontend
 # Both of them have max lenght, the friendly name it's not required (because blank=True)
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -26,7 +29,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    year = models.IntegerField(default=0)
+    year = models.IntegerField(default=2020)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
