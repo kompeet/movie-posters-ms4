95a4b0db5417107f052a24a4a8ba95cc3b220dcf
Peter Komaromi <kompeet1@gmail.com>
2020-12-17 15:35:09 +0000

    Added products app, categories.json added, app wired up in settings and admin

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+# Base of the models from the Boutique Ado project
+
+
+
+# Categories with a name and a friendly name for frontend
+# Both of them have max lenght, the friendly name it's not required (because blank=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+# Product model with key elements
+# Category, sku, image url and image fields are optional
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    year = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
