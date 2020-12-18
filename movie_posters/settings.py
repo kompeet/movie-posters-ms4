7b6e794d24f05fed7429717bdae07bc7fc6ae609
Peter Komaromi <kompeet1@gmail.com>
2020-12-18 15:52:10 +0000

    Bag context added with item, and delivery cost

--- movie_posters/settings.py
+++ movie_posters/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # allows django to access the HTTP request object in the template
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -154,3 +155,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 10
