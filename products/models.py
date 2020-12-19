119323317e45e08439a122e85bb09211180e0e31
Peter Komaromi <kompeet1@gmail.com>
2020-12-19 10:43:38 +0000

    Added product sizes and size selector box

--- products/models.py
+++ products/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     year = models.IntegerField(default=2020)
+    has_sizes = models.BooleanField(default=True, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
