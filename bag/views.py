570373d4602ce384b0c649d1f1a3c05becc2430a
Peter Komaromi <kompeet1@gmail.com>
2020-12-19 11:57:58 +0000

    Added adjust bag from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,24 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
-#View to render the bag itself
+# View to render the bag itself
 
 def view_bag(request):
-
-
+    """ A view that renders the bag contents page """
     return render(request, 'bag/bag.html')
-
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
-
     if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
@@ -32,8 +28,33 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
-            
+
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
 
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
