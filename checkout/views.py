73dbef02f51ba4456cc1560b2c027b7f46db216a
Peter Komaromi <kompeet1@gmail.com>
2020-12-21 19:20:02 +0000

    Stripe payment basics added

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HoppaA9qN4oyLnwWyvbazXaQHcSxCaCQwFUUw4bSXqhCugyBvTaEWs9fwJR8Kf7RoKCKjq1HexbNoMytcfB2dUy00CDBBZVV3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
