aa02dd3658ba126377bba6060e56bb731b560f81
Peter Komaromi <kompeet1@gmail.com>
2020-12-24 13:12:47 +0000

    Fixing the image field

--- products/forms.py
+++ products/forms.py
@@ -6,22 +6,13 @@ from .models import Product, Category
 class ProductForm(forms.ModelForm):
 
     class Meta:
-        '''
-        Metaclass defining the model and the fields
-        we want to use in ProductForm
-        Product model and all the fields included
-        '''
         model = Product
         fields = '__all__'
 
-        image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False,
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
-        '''
-        The superuser is able to add, edit and delete things
-        Categories displayed with their friendly names
-        Display field item with the matching styling attribute
-        '''
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
