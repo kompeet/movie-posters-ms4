bd5e737bf08064842f64f8e9475159b1b1f40083
Peter Komaromi <kompeet1@gmail.com>
2020-12-26 20:51:41 +0000

    Buttons styled

--- movie_posters/settings.py
+++ movie_posters/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'pages',
     'products',
     'bag',
     'checkout',
     'profiles',
+    
 
     # Other
     'crispy_forms',
@@ -216,9 +218,6 @@ STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET", "")
 
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'movieposters66@gmail.com'
-else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
@@ -226,3 +225,6 @@ else:
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
     DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'movieposters66@gmail.com'
