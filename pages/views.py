aea0c51495cc0401bc178db8e23baa858de6c9d1
Peter Komaromi <kompeet1@gmail.com>
2020-12-26 21:28:36 +0000

    Views refactored

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,6 @@ from django.template.loader import get_template
 from django.core.mail import EmailMessage
 from django.contrib import messages
 
-from products.models import Product
 from .forms import ContactForm
 
 
