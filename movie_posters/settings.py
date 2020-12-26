43fc69cf4cd98326858df6094e9d76c4cec20505
Peter Komaromi <kompeet1@gmail.com>
2020-12-26 14:27:29 +0000

    Installed djdatabse and psycopg2, start heroku deployment

--- movie_posters/settings.py
+++ movie_posters/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -123,12 +124,19 @@ WSGI_APPLICATION = 'movie_posters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL", ""))
+    }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
 
 
 # Password validation
