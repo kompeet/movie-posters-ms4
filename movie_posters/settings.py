44c81fe5141325dba6d0ceb0d9f5338bf3fc6ef5
Peter Komaromi <kompeet1@gmail.com>
2020-12-27 14:04:11 +0000

    Backend email adjusted

--- movie_posters/settings.py
+++ movie_posters/settings.py
@@ -218,6 +218,9 @@ STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET", "")
 
 
 if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'movieposters66@gmail.com'
+else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
@@ -225,6 +228,3 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
     DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'movieposters66@gmail.com'
