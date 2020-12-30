6166a11e32e70f2134bbb34c35f085266afd717d
Peter Komaromi <kompeet1@gmail.com>
2020-12-30 03:45:12 +0000

    Updated blog views

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,30 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import CommentForm
 from django.views import generic
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def all_posts(request):
     post_list = Post.objects.filter(status=1).order_by('-created_on')
-    template = "blog/blog.html"
+    paginator = Paginator(post_list, 5)  # 5 posts in each page
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+            # If page is not an integer deliver the first page
+        post_list = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        post_list = paginator.page(paginator.num_pages)
+
+    
     context = {
-        'post_list': post_list
+        'blog_page': 'active',
+        'page': page,
+        'post_list': post_list,
     }
-    return render(request, template, context)
+
+    return render(request, 'blog/blog.html', context)
 
 
 def post_detail(request, slug):
@@ -22,7 +37,6 @@ def post_detail(request, slug):
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-
             # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
             # Assign the current post to the comment
@@ -31,15 +45,7 @@ def post_detail(request, slug):
             new_comment.save()
     else:
         comment_form = CommentForm()
-
     return render(request, template_name, {'post': post,
                                            'comments': comments,
                                            'new_comment': new_comment,
                                            'comment_form': comment_form})
-    
-
-"""
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-"""
